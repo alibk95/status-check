96214b5549329ce176de046b74624f1b30bb0ac1
alibk95 <abkarimi1995@gmail.com>
2020-10-15 18:09:39 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# sudo pip3 install beautifulsoup4
+try:
+    from BeautifulSoup import BeautifulSoup
+except ImportError:
+    from bs4 import BeautifulSoup
+import requests
+
+
+def get(url):
+    data = requests.get(url)
+    # data_clean = data.content.decode("UTF-8")
+
+    parsed_html = BeautifulSoup(data.content, 'html.parser')
+    result = parsed_html.body.find('div', attrs={'class':"m-ok"}).text
+    print(result)
+    return result
+
+# alias per="python3 ~/permesso-di-soggiorno/main.py"
+url = 'https://questure.poliziadistato.it/stranieri/?mime=1&lang=english&pratica=061556289845'
+get(url)
+
